c49bcbf0b2dd0f06218f2eaeb9285cf26e0109bf
Hi Prasanth...!!! <pintuprasanth9@gmail.com>
2024-08-26 10:06:45 +0530

    create signin route

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,8 @@
 const User = require("../models/user.model");
 const bcryptjs = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const errorHandler = require("../utils/error");
 
 const signup = async (req, res , next) => {
   const { username, email, password } = req.body;
@@ -13,4 +16,21 @@ const signup = async (req, res , next) => {
     next(err);
   }
 };
-module.exports = { signup };
+const signin = async (req,res,next) =>{
+  const {email, password} = req.body;
+  try{
+    const validUser = await User.findOne({email});
+    if(!validUser) return next(errorHandler(404,'user not found'));
+    const validPassword = bcryptjs.compareSync(password,validUser.password);
+    if(!validPassword) return next(errorHandler(401,'wrong credentials'));
+    const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET);
+    const {password: hashedPassword , ...rest} = validUser._doc;
+    res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+    
+
+  }catch(err){
+    next(err);
+  }
+
+}
+module.exports = { signup ,signin};
