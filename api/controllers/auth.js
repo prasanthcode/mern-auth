ac29804566d51d844ac718f1b547c4628d0474de
Hi Prasanth...!!! <pintuprasanth9@gmail.com>
2024-08-26 10:39:57 +0530

    add signin page functionality

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -25,7 +25,7 @@ const signin = async (req,res,next) =>{
     if(!validPassword) return next(errorHandler(401,'wrong credentials'));
     const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET);
     const {password: hashedPassword , ...rest} = validUser._doc;
-    res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+    res.cookie('access_token', token, { httpOnly: true , maxAge: 10 * 60 * 1000 }).status(200).json(rest);
     
 
   }catch(err){
